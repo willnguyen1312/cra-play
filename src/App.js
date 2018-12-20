3641de8018351caad480de9a3b9b2284d3a335ec
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-12-20 12:05:02 +0700

    Waka Waka

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <header className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={styles.AppLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React {process.env.REACT_APP_SECRET_CODE}
           </a>
         </header>
       </div>
