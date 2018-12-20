94c4c1025229b5848711d4cf15c15ebd81a1289c
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-12-20 14:40:36 +0700

    Add CircleCI

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import styles from './App.module.css'
 
-class App extends PureComponent {
-  componentDidMount = () => {}
-
-  render() {
-    return (
-      <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles.AppLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            I love React {process.env.REACT_APP_SECRET_CODE}
-          </a>
-        </header>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className={styles.App}>
+    <header className={styles.AppHeader}>
+      <img src={logo} className={styles.AppLogo} alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className={styles.AppLink}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        I love React {process.env.REACT_APP_SECRET_CODE}
+      </a>
+    </header>
+  </div>
+)
 
 export default App
